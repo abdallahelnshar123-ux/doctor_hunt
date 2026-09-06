1b3dc372fa29b91dbf99aca335112997424338ac
abdallah elnshar <abdallah.elnshar.123@gmail.com>
2026-09-06 14:15:53 +0300

    finish admin login

--- lib/apps/features/auth/presentation/screens/admin_login_screen.dart
+++ lib/apps/features/auth/presentation/screens/admin_login_screen.dart
@@ -5,8 +5,13 @@ import 'package:doctor_hunt/apps/features/auth/presentation/widgets/auth_passwor
 import 'package:doctor_hunt/generated/style_atoms.dart';
 import 'package:doctor_hunt/generated/translations.g.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../../core/theme/app_colors.dart';
+import '../../../../core/utils/dialog_utils.dart';
+import '../controller/auth_bloc.dart';
+import '../controller/auth_event.dart';
+import '../controller/auth_state.dart';
 import '../widgets/custom_elevated_button.dart';
 import '../widgets/email_text_field_widget.dart';
 
@@ -32,62 +37,97 @@ class _AdminLoginScreenState extends State<AdminLoginScreen> {
   @override
   Widget build(BuildContext context) {
     final t = Translations.of(context);
-    return AppScaffold(
-      resizeToAvoidBottomInset: true,
-      body: Column(
-        children: [
-          MainAppBar(),
-          Expanded(
-            child: SingleChildScrollView(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              child: Form(
-                key: formKey,
-                child: Column(
-                  spacing: 8,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    SizedBox(height: 100),
-                    Text(
-                      t.auth.welcome_back,
-                      style: context.medium24.black.rubik,
-                      textAlign: TextAlign.center,
-                    ),
-                    Text(
-                      t.auth.admin_subtitle,
-                      style: context.regular14.textSecondary.rubik,
-                      textAlign: TextAlign.center,
-                    ),
-                    // SizedBox(height: 70),
-                    // Row(
-                    //   spacing: 15,
-                    //   children: [
-                    //     Expanded(
-                    //       child: ContinueWithGoogleButton(onPressed: () {}),
-                    //     ),
-                    //     Expanded(
-                    //       child: ContinueWithFacebookButton(onPressed: () {}),
-                    //     ),
-                    //   ],
-                    // ),
-                    SizedBox(height: 30),
-                    EmailTextFieldWidget(
-                      controller: emailController,
-                      fillColor: AppColors.bgPrimary,
-                    ),
-                    AuthPasswordTextFieldWidget(controller: passwordController),
-                    SizedBox(height: 24),
-                    _builtLoginButton(),
-                    _buildForgetPassword(),
-                  ],
+    return BlocListener<AuthBloc, AuthState>(
+      listener: (context, state) {
+        if (state is UserAuthenticatedState) {
+          DialogUtils.hideLoading(context: context);
+          DialogUtils.showMessage(
+            title: 'success',
+            context: context,
+            message: 'success',
+          );
+
+          Future.delayed(Duration(seconds: 2), () {
+            if (context.mounted) {
+              const AdminMainRoute().go(context);
+            }
+          });
+        }
+
+        if (state is LoginWithEmailPasswordErrorState) {
+          debugPrint(state.message);
+          DialogUtils.hideLoading(context: context);
+          DialogUtils.showMessage(
+            posActionText: 'ok',
+            title: 'error',
+            context: context,
+            message: state.message,
+          );
+        }
+        if (state is LoginWithEmailPasswordLoadingState) {
+          DialogUtils.showLoading(context: context);
+        }
+      },
+
+      child: AppScaffold(
+        resizeToAvoidBottomInset: true,
+        body: Column(
+          children: [
+            MainAppBar(),
+            Expanded(
+              child: SingleChildScrollView(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Form(
+                  key: formKey,
+                  child: Column(
+                    spacing: 8,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SizedBox(height: 100),
+                      Text(
+                        t.auth.welcome_back,
+                        style: context.medium24.black.rubik,
+                        textAlign: TextAlign.center,
+                      ),
+                      Text(
+                        t.auth.admin_subtitle,
+                        style: context.regular14.textSecondary.rubik,
+                        textAlign: TextAlign.center,
+                      ),
+                      // SizedBox(height: 70),
+                      // Row(
+                      //   spacing: 15,
+                      //   children: [
+                      //     Expanded(
+                      //       child: ContinueWithGoogleButton(onPressed: () {}),
+                      //     ),
+                      //     Expanded(
+                      //       child: ContinueWithFacebookButton(onPressed: () {}),
+                      //     ),
+                      //   ],
+                      // ),
+                      SizedBox(height: 30),
+                      EmailTextFieldWidget(
+                        controller: emailController,
+                        fillColor: AppColors.bgPrimary,
+                      ),
+                      AuthPasswordTextFieldWidget(
+                        controller: passwordController,
+                      ),
+                      SizedBox(height: 24),
+                      _builtLoginButton(),
+                      _buildForgetPassword(),
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-          // Padding(
-          //   padding: EdgeInsets.only(bottom: 25),
-          //   child: _buildDoNotHaveAccount(),
-          // ),
-        ],
+            // Padding(
+            //   padding: EdgeInsets.only(bottom: 25),
+            //   child: _buildDoNotHaveAccount(),
+            // ),
+          ],
+        ),
       ),
     );
   }
@@ -130,8 +170,14 @@ class _AdminLoginScreenState extends State<AdminLoginScreen> {
     return CustomElevatedButton(
       buttonWidth: MediaQuery.sizeOf(context).width - 80,
       onPressed: () {
-        const AdminMainRoute().go(context);
-      },
+        if (formKey.currentState?.validate() ?? false) {
+          context.read<AuthBloc>().add(
+            LoginRequested(
+              email: emailController.text,
+              password: passwordController.text,
+            ),
+          );
+        }      },
       backgroundColor: AppColors.brandPrimary,
       child: Text(t.auth.login, style: context.medium18.white.rubik),
     );
